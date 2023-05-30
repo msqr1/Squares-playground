1ad2e89470a5f5f42b66f7ca8f071afcbacfbb58
msqr1 <134968689+msqr1@users.noreply.github.com>
2023-05-30 16:47:45 -0700

    Update Square.h
    Optimize struct Square for memory alignment

--- Multiple Squares/Square.h
+++ Multiple Squares/Square.h
@@ -6,12 +6,12 @@ struct Square
 {
 	Square(const char* name, uint_least8_t index, uint_least8_t r, uint_least8_t g, uint_least8_t b);
 	Square() = default;
+	char name[16]{};
+	double posx{};
+	double posy{};
 	uint_least8_t r{};
 	uint_least8_t g{};
 	uint_least8_t b{};
-	double posx{};
-	double posy{};
 	uint_least8_t index{};
-	char name[16]{};
 };
 typedef std::unordered_map <uint_least8_t, std::unique_ptr<Square>> Sqrc;
